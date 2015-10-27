8e629e33383af60a35635f4eea2864a6600d6166
Jason Fleagle <jasonfleagle@hotmail.com>
2015-10-27 03:38:32 +0000

    Made changes for RSS feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,11 @@ class HomeController < ApplicationController
   def home
       @links = Link.all
   end
+  
+  def feed
+    @blog_articles = BlogArticle.all
+    respond_to do |format|
+      format.rss { render :layout => false }
+    end
+  end
 end
